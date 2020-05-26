c9e48535bfb9fecdf674080505610f601d1eeed0
akky-m <akiko30502376@gmail.com>
2020-05-26 12:00:00 +0000

    timeline

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,14 @@
 |
 */
 
+
 Route::get('/', 'MicropostsController@index');
 
 Route::group(['middleware' => ['auth']], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
     Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
+
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
