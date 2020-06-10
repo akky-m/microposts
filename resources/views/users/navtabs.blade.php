0b392d94182d1b47acc5a424b981a5bfa9507ede
akky-m <akiko30502376@gmail.com>
2020-06-10 13:35:19 +0000

    complete14

--- resources/views/users/navtabs.blade.php
+++ resources/views/users/navtabs.blade.php
@@ -3,25 +3,25 @@
  <li class="nav-item">
         <a href="{{ route('users.show', ['id' => $user->id]) }}"
             class="nav-link {{ Request::is('users/' . $user->id) ? 'active' : '' }}">TimeLine
-            <span class="badge badge-secondary">@if(!empty($count_microposts)){{ $count_microposts }}@endif</span>
+            <span class="badge badge-secondary">@if(!empty($data['count_microposts'])){{ $data['count_microposts'] }}@endif</span>
                      </a>
  </li>
     <li class="nav-item">
         <a href="{{ route('users.followings', ['id' => $user->id]) }}"
             class="nav-link {{ Request::is('users/*/followings') ? 'active' : '' }}">Followings
-          <span class="badge badge-secondary">@if(!empty($count_followings)){{ $count_followings }}@endif</span>
+          <span class="badge badge-secondary">@if(!empty($data['count_followings'])){{ $data['count_followings'] }}@endif</span>
         </a>
     </li>
     <li class="nav-item">
        <a href="@if(!empty($user)){{ route('users.followers', ['id' => $user->id]) }}@endif"
             class="nav-link {{ Request::is('users/*/followers') ? 'active' : '' }}">Followers
-           <span class="badge badge-secondary">@if(!empty($count_followers)){{ $count_followers }}@endif</span>
+           <span class="badge badge-secondary">@if(!empty($data['count_followers'])){{ $data['count_followers'] }}@endif</span>
         </a>
     </li>
     <li class="nav-item">
         <a href="{{ route('users.favorites', ['id' => $user->id]) }}" class="nav-link
     {{ Request::is('users/*/favorites') ? 'active' : '' }}">Favorite_article
-     <span class="badge badge-secondary">@if(!empty($count_favorites)){{ $count_favorites }@endif</span>
+     <span class="badge badge-secondary">@if(!empty($data['count_favorites'])){{ $data['count_favorites'] }}@endif</span>
         </a>
     </li>
 </ul> 
